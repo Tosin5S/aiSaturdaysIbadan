27cac8e2e2ca6e147c2cd661d9f93ecdc8006484
Tosin5S <olyguy@gmail.com>
2023-04-15 18:06:09 +0100

    intro: python basics

---
+++ week0/intro.py
@@ -0,0 +1,48 @@
+#####################################
+# AI Saturday Introduction to Python and Data Science
+x = 5
+_x = 3.142
+
+print ("x is : ", x, "and _x is ", _x)
+
+x = "5"
+y = "10"
+
+t = 5
+k = 10
+
+z  = x + y 
+w = t + k 
+
+d = int(x) + int(y)
+
+print ("z: ", z , "w: " , w)
+
+print("WELCOME TO PYTHON PROGRAMMING")
+name = input("What is your name: ")
+print("Your name is " , name)
+
+radius = input("Enter the radius r: ")
+r = int(radius)
+a = 3.142 * r ** 2
+print("The area of the circle = ", a)
+
+
+# classwork: calculate the volume of a cone
+# r = int(input("Enter the .... ")
+
+#solution
+
+# v = 1/3 * 3.142 * r **2 *h
+h = int( input ("Enter the height of a cone: "))
+r = int (input("Enter the radius of a cone: "))
+
+v = 1/3 * 3.142 * r**2 *h
+
+print("The volume of a cone is : ", v)
+
+############################################
+# Continuation
+
+
+
---
+++ week0/loopsAndSelection.py
@@ -0,0 +1,46 @@
+# Repitition (Loops) and Selection (if/else)
+# It is part of the core aspect
+
+# if (condition):
+   # statement:
+
+gpa = 3.142
+
+if (gpa > 2.5):
+    print("Your class of degree is above 2.2")
+
+else: 
+    print("Your class of degree is ...")
+
+print ("DISPLAY STUDENT CLASS OF DEGREE")
+
+cgpa = float(input("Enter your cgpa: "))
+if (cgpa >= 4.5):
+    print("You are a first class student")
+elif((cgpa >= 3.5) and (cgpa < 4.5)):
+    print("You are a second class upper student")
+elif((cgpa >= 2.5) and (cgpa < 3.49)):
+    print("You are a second class lower student")
+elif((cgpa >= 1.5) and (cgpa < 2.49)):
+    print("You are a third class student")
+else:
+    print("You will have to go to your house (lol)")
+
+###################################################
+# Control Structures or Repetitive Statements
+
+x = 1
+while (x <= 100):
+    print (x)
+    x = x + 1
+
+# Even numbers between 1 to 100
+
+x = 2
+while (x <= 100):
+    print (x)
+    x = x + 2
+
+# assignment 1: print a prime numbers between 1 and 100
+# assignment 2: calculate n factorial
+# assignment 3: use math.pi to calculate the volume of a cone
